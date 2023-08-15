ef0df83e3741fb25db0a6e04a7eb1e6b565b3386
cperez1012 <50646333+cperez1012@users.noreply.github.com>
2023-08-15 01:20:09 -0400

    removed unnecessary ts file, updating routing to include HttpClientModule in app.module.ts, updated login component with create account and toggle between both login and create account forms

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  sayHello() {
-    return this.http.get<string>(`${this.apiUrl}/hello`);
+  login(credentials: any): Observable<any> {
+    return this.http.post(this.apiUrl, credentials);
   }
 }
